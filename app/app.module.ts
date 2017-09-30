dec500204068765713cf1ef47e32f6a5ae3b3a91
trugoj <martin@ganymed.org>
2017-09-30 17:54:38 +0200

    initial setup

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    import:         [BrowserModule],
+    declarations:   [AppComponent],
+    bootstrap:      [AppComponent]
+})
+export class AppModule {}
